7c3fec22011621f1d2aab01eccc23250e135d566
Daniel Hoang <thesubway@gmail.com>
2016-06-10 15:29:47 -0700

    Add Order model with ref to line_items and validations

---
+++ app/models/order.rb
@@ -0,0 +1,6 @@
+class Order < ActiveRecord::Base
+  has_many :line_items,dependent: :destroy
+  PAYMENT_TYPES = ["Check", "Credit Card", "Money Order"]
+  validates :name, :address, :email, presence: true
+  validates :pay_type, inclusion: PAYMENT_TYPES
+end
