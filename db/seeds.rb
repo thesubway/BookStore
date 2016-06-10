8ec025e87c2d40a6d151fcfb6a310bc35c0e8f8f
Daniel Hoang <thesubway@gmail.com>
2016-06-09 22:21:25 -0700

    Delete and add products with seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Product.delete_all
+
+Product.create!(name: 'CoffeeScript',
+
+  description:
+
+    %{<p>
+
+        CoffeeScript is JavaScript done right. It provides all of JavaScript's
+
+  functionality wrapped in a cleaner, more succinct syntax. In the first
+
+  book on this exciting new language, CoffeeScript guru Trevor Burnham
+
+  shows you how to hold onto all the power and flexibility of JavaScript
+
+  while writing clearer, cleaner, and safer code.
+
+      </p>},
+
+  image:   'cs.jpg',
+
+  price: 36.00)
