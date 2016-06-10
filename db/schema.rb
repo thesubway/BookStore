8622837da6ec6e88df03551e7e5a892ce873eab9
Daniel Hoang <thesubway@gmail.com>
2016-06-10 13:52:24 -0700

    Create empty migration but fill in to combine items

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160610203929) do
+ActiveRecord::Schema.define(version: 20160610204918) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
